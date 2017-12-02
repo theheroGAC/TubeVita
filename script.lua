368b51e0246188780852404112e01308e9c0d8aa
theheroGAC <gamesandconsolestaff@gmail.com>
2017-12-02 16:17:34 +0100

    v1.01

--- script.lua
+++ script.lua
@@ -1,1 +1,23 @@
-os.uri ("webmodal: https://m.youtube.com")
+--[[
+	TubeVita 1.01
+	Creato da TheheroGAC (GAMESANDCONSOLES.ORG) sab 2 dec 2017 15:23:47 CET 
+	Licensed by Creative Commons Attribution-ShareAlike 4.0
+	http://creativecommons.org/licenses/by-sa/4.0/
+
+	# script.lua
+	#
+	# TubeVita è un'applicazione creata con ONELua (grazie al Team OneLua) per vedere i video di Youtube su PSVITA
+	# Sentitevi liberi di modificare il codice sorgente
+]]
+
+-- ## MESSAGGIO DI CARICAMENTO ##
+screen.print(5, 5, 'Loading Tubevita...'); screen.flip()
+splash.show("resources/tubevita.png")
+
+-- ## MENU' PRINCIPALE ##
+while true do
+	-- Controlli e prossimi aggiornamenti
+	buttons.read()
+	screen.flip()
+	color.loadpalette()
+os.uri ("webmodal: https://m.youtube.com") end
