2a7490f9c789baf5a9fdf53063733c212b0a2dc2
theheroGAC <gamesandconsolestaff@gmail.com>
2017-12-03 11:19:28 +0100

    v1.02

--- script.lua
+++ script.lua
@@ -14,13 +14,32 @@
 dofile("git/updater.lua")
 
 -- ## MESSAGGIO DI CARICAMENTO ##
+game.close()
 screen.print(5, 5, 'Loading Tubevita...'); screen.flip()
 splash.show("resources/tubevita.png")
 
+-- ## IMMAGINE DI SFONDO ##
+back = image.load("resources/back.png")
+
+
 -- ## MENU' PRINCIPALE ##
 while true do
-	-- Controlli e prossimi aggiornamenti
 	buttons.read()
-	screen.flip()
+if back then back:blit(0,0) end
 	color.loadpalette()
-os.uri ("webmodal: https://m.youtube.com") end
+screen.print(910,30,"TubeVita",1,color.white,color.blue,__ARIGHT)
+	screen.print(910,60,"v1.02",1,color.white,color.blue,__ARIGHT)
+
+-- ## Controlli ##
+
+screen.print(25, 40, "YouTube",1, color.red, color.black)
+screen.print(25, 65, "Playstation Channel",1, color.red, color.black)
+screen.print(25, 446, "Press X for Youtube",1, color.red, color.black)
+screen.print(25, 468, "Press Square for Playstation Channel",1, color.red, color.black)
+screen.print(25, 490, "Press Triangle for Exit ",1, color.red, color.black)
+
+if buttons.cross then os.uri ("webmodal: https://m.youtube.com") end
+if buttons.square then os.uri ("webmodal: https://www.youtube.com/user/PlayStation/videos") end
+if buttons.triangle then os.exit() end
+screen.flip()
+end
